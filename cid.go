23b5891aa74629d3375cd0fbf02ac0d2be2375dc
albert <zhuliting@gxb.io>
2018-04-08 17:56:17 +0800

    update cid

--- cid.go
+++ cid.go
@@ -15,19 +15,7 @@ import (
 	ft "github.com/ipfs/go-ipfs/unixfs"
 )
 
-func main() {
-	var fname, raw_string string
-	flag.StringVar(&fname, "f", "", "file to be added")
-	flag.StringVar(&raw_string, "s", "", "string to be added")
-	flag.Parse()
-
-	// fmt.Println("fname:", fname)
-	// fmt.Println("raw_string:", raw_string)
-
-	if raw_string == "" && fname == "" {
-		os.Exit(1)
-		return
-	}
+func get_cid(data []byte) {
 
 	// ipfsnode
 	ctx := context.TODO()
@@ -55,31 +43,37 @@ func main() {
 	}
 	fileAdder.SetMfsRoot(mr)
 
+	root, err := fileAdder.Myadd(bytes.NewReader(data))
+	if err != nil {
+		os.Exit(5)
+		return
+	}
+
+	ipfs_cid := root.Cid().String()
+	fmt.Printf("{\"cid\":\"%s\"}", ipfs_cid)
+}
+
+func main() {
+	var fname, raw_string string
+	flag.StringVar(&fname, "f", "", "file to be added")
+	flag.StringVar(&raw_string, "s", "", "string to be added")
+	flag.Parse()
+
+	// fmt.Println("fname:", fname)
+	// fmt.Println("raw_string:", raw_string)
+
+	if raw_string == "" && fname == "" {
+		os.Exit(1)
+		return
+	}
+
 	if len(raw_string) > 0 {
 		data := []byte(raw_string)
 		// fmt.Println("data:", data)
-
-		root, err := fileAdder.Myadd(bytes.NewReader(data))
-
-		if err != nil {
-			os.Exit(5)
-			return
-		}
-
-		ipfs_cid := root.Cid().String()
-		fmt.Printf("{\"cid\":\"%s\"}", ipfs_cid)
+		get_cid(data)
 	} else if len(fname) > 0 {
 		data, _ := ioutil.ReadFile(fname)
 		// fmt.Println("data:", data)
-
-		root, err := fileAdder.Myadd(bytes.NewReader(data))
-
-		if err != nil {
-			os.Exit(5)
-			return
-		}
-
-		ipfs_cid := root.Cid().String()
-		fmt.Printf("{\"cid\":\"%s\"}", ipfs_cid)
+		get_cid(data)
 	}
 }
