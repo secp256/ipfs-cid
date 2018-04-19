637c14daeab156ca1cfa4bc1cefee1027ce1f622
albert <zhuliting@gxb.io>
2018-04-19 15:08:17 +0800

    add cid_v2

---
+++ cid_v2.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"unsafe"
+
+	core "github.com/ipfs/go-ipfs/core"
+	balanced "github.com/ipfs/go-ipfs/importer/balanced"
+	ihelper "github.com/ipfs/go-ipfs/importer/helpers"
+	chunker "gx/ipfs/QmWo8jYc19ppG7YoTsrr2kEtLRbARTJho5oNXFTR6B7Peq/go-ipfs-chunker"
+)
+
+func get_cid(data []byte) {
+
+	// ipfsnode
+	node, err := core.NewNode(context.Background(), &core.BuildCfg{
+		NilRepo: true,
+	})
+	if err != nil {
+		os.Exit(2)
+		return
+	}
+	// defer node.Close()
+
+	chnk, err := chunker.FromString(bytes.NewReader(data), "")
+	if err != nil {
+		os.Exit(3)
+		return
+	}
+
+	params := ihelper.DagBuilderParams{
+		Dagserv:   node.DAG,
+		RawLeaves: false,
+		Maxlinks:  ihelper.DefaultLinksPerBlock,
+		NoCopy:    false,
+	}
+
+	root, err := balanced.Layout(params.New(chnk))
+	if err != nil {
+		os.Exit(4)
+		return
+	}
+
+	fmt.Printf("{\"cid\":\"%s\"}", root.Cid().String())
+}
+
+func str_to_bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+func main() {
+	var fname, raw_string string
+	flag.StringVar(&fname, "f", "", "file to be added")
+	flag.StringVar(&raw_string, "s", "", "string to be added")
+	flag.Parse()
+
+	if raw_string == "" && fname == "" {
+		get_cid(str_to_bytes(""))
+		return
+	}
+
+	if len(raw_string) > 0 {
+		data := str_to_bytes(raw_string)
+		// data := []byte(raw_string)
+		get_cid(data)
+	} else if len(fname) > 0 {
+		data, _ := ioutil.ReadFile(fname)
+		// fmt.Println(len(data))
+		get_cid(data)
+	}
+}
